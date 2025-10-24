4ed3e9a1999250c495287752d064d577e4dfdefb
sanchezmatias911 <sanchezmatias911@yahoo.com>
2025-10-23 23:19:55 -0300

    Avances de Buque y coordenada

--- Terminal_Portuaria/src/ar/edu/unq/po2/Coordenada/Coordenada.java
+++ Terminal_Portuaria/src/ar/edu/unq/po2/Coordenada/Coordenada.java
@@ -10,28 +10,42 @@ public class Coordenada {
 
     private double latidud
     private double longitud
+
     /*
-    * Constructor de Coordenada
+    * Constructor de Coordenada:
+    * @param latitud longitud : en conjunto representan la ubicacion geográfica
+    * del usuario que la usa
     * */
     public Coordenada(double latitud, double longitud){
         this.latidud=latitud;
         this.longitud=longitud;
     }
+
+
+    private double getLatitud() { return latitud; }
+    private double getLongitud() { return longitud; }
+
+    private void setLatitud(double latitud) { this.latitud = latitud; }
+    private void setLongitud(double longitud) { this.longitud = longitud; }
+
     /*
-    *
-    * */
-    public double getLatitud() { return latitud; }
-    public double getLongitud() { return longitud; }
-
-    public void setLatitud(double latitud) { this.latitud = latitud; }
-    public void setLongitud(double longitud) { this.longitud = longitud; }
-
-    public posicionActual(double latitud, double longitud){
+     * Precondicion: la latitud y longitud deben ser razonables
+     * @param latitud  longitud: en conjunto reprentan la nueva ubicacion geográfica
+     *                           del usuario
+     */
+    public nuevaPosicion(double latitud, double longitud){
         /*
         * Precondicion: la latitud y longitud deben ser razonables
         * */
         this.latidud=latitud;
         this.longitud=longitud;
+    }
+    /*
+    * @return devuelve un par que representa la posicion geográfica actual
+    * del usuario
+    * */
+    public Pair<double,double>posicionActual(){
+        return new Pair<>(latidud,longitud);
     }
 
 }
