c570e53361f986f6de0b30987596528943f045f4
sanchezmatias911 <sanchezmatias911@yahoo.com>
2025-10-25 16:45:50 -0300

    agregado de } es un archivito

--- Terminal_Portuaria/src/ar/edu/unq/po2/container/Container.java
+++ Terminal_Portuaria/src/ar/edu/unq/po2/container/Container.java
@@ -17,11 +17,11 @@ public abstract class Container {
 
     private double ancho;
 
-   private double largo;
+    private double largo;
 
-   private double altura;
+    private double altura;
 
-   private double peso;
+    private double peso;
 
     private List<TipoServicio> servicios;
 
@@ -33,6 +33,7 @@ public abstract class Container {
     public String getIdConnteiner() {
         return idConnteiner;
     }
+
     public double getAncho() {
         return ancho;
     }
@@ -56,48 +57,48 @@ public abstract class Container {
 
     // ********************************* CONSTRUCTOR ********************************
     /*
-    * Constructor de conteiner
-    * @param duenioConsignee representa al dueño de la carga. Por lo general sera un consignee
-    * */
-    public Container(Cliente duenio, double ancho, double largo, double altura, double peso){
-        this.duenioConsignee=duenio;
-        this.ancho=ancho;
-        this.largo=largo;
-        this.altura=altura;
-        this.peso=peso;
+     * Constructor de conteiner
+     * @param duenioConsignee representa al dueño de la carga. Por lo general sera un consignee
+     * */
+    public Container(Cliente duenio, double ancho, double largo, double altura, double peso) {
+        this.duenioConsignee = duenio;
+        this.ancho = ancho;
+        this.largo = largo;
+        this.altura = altura;
+        this.peso = peso;
         this.idConteiner(this.getDuenioConsignee().nombreCliente());
     }
 
     /*
-    * Proposito: crea un nuevo id para el conteiner, conformado por las primeras 4 letras en mayusculas del cliente
-    * seguido de 7 numero
-    * @param cliente: representa al duenio de la carga
-    * */
-    protected void idConteiner(String cliente){
-        String nuevaClave = this.codigoCliente(cliente) +  this.codigoNumericoRandom();
+     * Proposito: crea un nuevo id para el conteiner, conformado por las primeras 4 letras en mayusculas del cliente
+     * seguido de 7 numero
+     * @param cliente: representa al duenio de la carga
+     * */
+    protected void idConteiner(String cliente) {
+        String nuevaClave = this.codigoCliente(cliente) + this.codigoNumericoRandom();
         this.idConnteiner = nuevaClave;
     }
 
     /*
-    * @Return : devuelve las priemras 4 letras del nombre del cliente en Mayusculas
-    * */
-    protected String codigoCliente(String cliente){
+     * @Return : devuelve las priemras 4 letras del nombre del cliente en Mayusculas
+     * */
+    protected String codigoCliente(String cliente) {
         return cliente.substring(0, 3).toUpperCase();
     }
 
-    protected String codigoNumericoRandom(){
+    protected String codigoNumericoRandom() {
         int numero = ThreadLocalRandom.current().nextInt(0, 10_000_000); // numero aleatorio entre 0 a 9,999,999
-        return  this.num7Caracteres(numero);
+        return this.num7Caracteres(numero);
     }
 
     /*
-    * @Return dado un numero, devuelve una cadena de caracteres de 7 digitos, rellenando con 0 a la izquierda
-    * de ser necesario
-    * */
-    protected String num7Caracteres(int num){
+     * @Return dado un numero, devuelve una cadena de caracteres de 7 digitos, rellenando con 0 a la izquierda
+     * de ser necesario
+     * */
+    protected String num7Caracteres(int num) {
         return String.format("%07d", num);
     }
-
+}
 
 
     // *****************************************************************
