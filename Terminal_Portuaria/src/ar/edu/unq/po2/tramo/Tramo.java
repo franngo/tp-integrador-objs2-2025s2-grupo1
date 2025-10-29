998d5352f14e074c9e377cd717ced4248385d57b
sanchezmatias911 <sanchezmatias911@yahoo.com>
2025-10-28 23:15:18 -0300

    creacion clase Tramo

---
+++ Terminal_Portuaria/src/ar/edu/unq/po2/tramo/Tramo.java
@@ -0,0 +1,20 @@
+package ar.edu.unq.po2.tramo;
+
+import ar.edu.unq.po2.terminal_portuaria.TerminalPortuaria;
+
+import java.time.Duration;
+
+
+/*
+* @Autor: Matias Sanchez
+* */
+public class Tramo {
+
+    TerminalPortuaria terminalDestino;
+    TerminalPortuaria terminalOrigen;
+
+    Duration tiempoTotal;
+    double precioTramo;
+
+
+}
