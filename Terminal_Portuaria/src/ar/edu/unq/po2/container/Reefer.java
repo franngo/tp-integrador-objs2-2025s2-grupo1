bcfec304e04cd9b68869fb3d19690f0399979eea
sanchezmatias911 <sanchezmatias911@yahoo.com>
2025-11-01 17:19:48 -0300

    avance implementacion de servicios

--- Terminal_Portuaria/src/ar/edu/unq/po2/container/Reefer.java
+++ Terminal_Portuaria/src/ar/edu/unq/po2/container/Reefer.java
@@ -6,12 +6,17 @@ import ar.edu.unq.po2.cliente.Cliente;
 import ar.edu.unq.po2.servicio.*;
 
 public class Reefer extends Container{
+	private double consumoPorHora;
 
-	public Reefer(Cliente duenio, double ancho, double largo, double altura, double peso) {
+	public Reefer(Cliente duenio, double ancho, double largo, double altura, double peso, double consumoPorHora) {
 		super(duenio, ancho, largo, altura, peso);
+		this.consumoPorHora=consumoPorHora;
 		// TODO Auto-generated constructor stub
 	}
-
+    
+	public double getConsumoPorHora() {
+		return consumoPorHora;
+	}
 	@Override
 	public List<Servicio> acceptVisitor(ConcreteVisitorContainer visitante) {
 		// TODO Auto-generated method stub
