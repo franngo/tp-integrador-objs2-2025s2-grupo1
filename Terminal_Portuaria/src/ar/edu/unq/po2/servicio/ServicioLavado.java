ff747c7a33dc26f43023aedaacae11b1675b4d1d
sanchezmatias911 <sanchezmatias911@yahoo.com>
2025-11-02 12:11:55 -0300

    avance servicio lavado

--- Terminal_Portuaria/src/ar/edu/unq/po2/servicio/ServicioLavado.java
+++ Terminal_Portuaria/src/ar/edu/unq/po2/servicio/ServicioLavado.java
@@ -3,7 +3,12 @@ package ar.edu.unq.po2.servicio;
 import ar.edu.unq.po2.container.Container;
 import ar.edu.unq.po2.terminal_portuaria.TerminalPortuaria;
 
+
+/*
+ * @Autor: Matias Sanchez
+ * */
 public class ServicioLavado extends Servicio{
+	
 	public ServicioLavado(Container containerServ) {
 		super(containerServ);
 		// TODO Auto-generated constructor stub
@@ -15,7 +20,23 @@ public class ServicioLavado extends Servicio{
 	@Override
 	public double costoServicio(TerminalPortuaria terminalPortuaria) {
 		// TODO Auto-generated method stub
-		return 0;
+		if(this.superaCapacidad()) {
+			return terminalPortuaria.precioServicio(PrecioServicioTerminal.LAVADOPESADO);
+		}
+		else {return terminalPortuaria.precioServicio(PrecioServicioTerminal.LAVADOCOMUN);}
 	}
 	
+	public double volumenContainer() {
+		return containerServ.getAltura() 
+			   * containerServ.getAncho() 
+			   * containerServ.getLargo();
+	}
+	
+	public double volumenMaximo() {
+		return 70.0d;
+	}
+	
+	public boolean superaCapacidad() {
+		return this.volumenContainer()>this.volumenMaximo();
+	}
 }
