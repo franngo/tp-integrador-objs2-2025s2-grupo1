cb10cb47b41855fe2c967c25e7042887be72f601
Fran <155672163+franngo@users.noreply.github.com>
2025-11-05 22:39:37 -0300

    Implementación de las clases relacionadas al buscador de circuito. Falta testing.

---
+++ Terminal_Portuaria/src/ar/edu/unq/po2/buscador_de_circuito/BuscadorDeCircuito.java
@@ -0,0 +1,15 @@
+package ar.edu.unq.po2.buscador_de_circuito;
+
+import java.util.List;
+
+import ar.edu.unq.po2.circuito_maritimo.CircuitoMaritimo;
+
+public interface BuscadorDeCircuito {
+	
+	/*
+	 * Ya se le pasa directamente la lista de circuitos entre la terminal gestionada y la otra terminal de la consulta.
+	 * No hace falta realizar ese filtro sobre la lista de circuitos acá.
+	 */
+	public CircuitoMaritimo buscarCircuito(List<CircuitoMaritimo> circuitos);
+	
+}
