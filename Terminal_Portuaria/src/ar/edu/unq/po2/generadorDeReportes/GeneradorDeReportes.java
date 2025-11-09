fa1a294f4439efc13ebd2ca470cbeb83ccbc5ee3
Fran <155672163+franngo@users.noreply.github.com>
2025-11-09 11:39:56 -0300

    Avance en la implementación de los métodos de generación de reportes.

--- Terminal_Portuaria/src/ar/edu/unq/po2/generadorDeReportes/GeneradorDeReportes.java
+++ Terminal_Portuaria/src/ar/edu/unq/po2/generadorDeReportes/GeneradorDeReportes.java
@@ -1,7 +1,9 @@
 package ar.edu.unq.po2.generadorDeReportes;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import ar.edu.unq.po2.buque.Buque;
 import ar.edu.unq.po2.orden.Orden;
@@ -16,17 +18,23 @@ public class GeneradorDeReportes {
 		this.visitorB = new VisitorReporteBuque();
 	}
 		
-	protected List<Reporte> generarReportesConImportaciones(Buque buque, List<Orden> ordenes) {
+	protected Map<String,Reporte> generarReportesConImportaciones(Buque buque, List<Orden> ordenes) {
 		
 		String nombreBuque = buque.getNombre();
 		String fechaPartida = buque.getViajeActual().fechaDeSalida().toString();
 		String fechaArribo = buque.getViajeActual().fechaDeLlegada().toString();
+		
+		///////////////////////Reporte muelle/////////////////////////
 		
 		String muelle = "Nombre del buque: " + nombreBuque +"\n"
 					+"Fecha de partida: " + fechaPartida + "\n"
 					+"Fecha de arribo: " + fechaArribo + "\n"
 					+"Contenedores descargados: " + ordenes.size() + "\n";
 		Reporte r1 = new Reporte(muelle);
+		
+		///////////////////////Reporte muelle/////////////////////////
+		
+		///////////////////////Reporte aduana/////////////////////////
 		
 		String aduana = "<html lang=\"en\">\n"
 				+ "    <head>\n"
@@ -44,27 +52,89 @@ public class GeneradorDeReportes {
 			aduana = aduana + orden.accept(visitorA);
 		}
 		
-		aduana = aduana
-				+ "    		</ul>"
-				+ "    </body>"	
-				+ "</html>";	
 		Reporte r2 = new Reporte(aduana);
+		
+		///////////////////////Reporte aduana/////////////////////////
 			
+		///////////////////////Reporte buque/////////////////////////
 		
-		//<li><p> ... </p></li>
+		String repBuque = "<report>\n"
+					+	  "		<import>\n";
 		
-		String repBuque = "<report>";
+		for(Orden orden : ordenes) {
+			repBuque = repBuque + orden.accept(visitorB);
+		}
 		
-		List<Reporte> rs = new ArrayList<Reporte>();
-		rs.add(r1);
-		rs.add(r2);
-		//rs.add(r3);
+		repBuque = repBuque +	"		</import>\n";
+		
+		Reporte r3 = new Reporte(repBuque);
+		
+		///////////////////////Reporte buque/////////////////////////
+		
+		Map<String,Reporte> rs = new HashMap<String,Reporte>();
+		rs.put("muelle", r1);
+		rs.put("aduana", r2);
+		rs.put("buque", r3);
 		
 		return rs;
 				
 	}
 	
-	protected List<Reporte> finalizarReportesConExportaciones(List<Reporte> reportes, List<Orden> ordenes) {
+	protected List<Reporte> finalizarReportesConExportaciones(Map<String,Reporte> reportes, List<Orden> ordenes) {
+		
+		///////////////////////Reporte muelle/////////////////////////
+		///
+		String muelle = "Contenedores cargados: " + ordenes.size();
+		
+		Reporte r1 = reportes.get("muelle"); //el reporte muelle con únicamente la información de importaciones
+		r1.setTexto(r1.getTexto() + muelle);
+		
+		///////////////////////Reporte muelle/////////////////////////
+		
+		///////////////////////Reporte aduana/////////////////////////
+		
+		String aduana = "<html lang=\"en\">\n"
+				+ "    <head>\n"
+				+ "        <meta charset=\"utf-8\">\n"
+				+ "        <title>Reporte aduana</title>\n"
+				+ "    </head>\n"
+				+ "    <body>"
+				+ "	   		<h2>Nombre del buque: " + nombreBuque +"</h2>\n"
+				+ "    		<h1>Fecha de partida: " + fechaPartida + "</h1>\n"
+				+ "    		<h1>Fecha de arribo: " + fechaArribo + "</h1>\n"
+				+ "	   		<h2>Lista de contenedores (con su tipo y ID): </h2>\n"
+				+ "    		<ul>";
+		
+		for(Orden orden : ordenes) {
+			aduana = aduana + orden.accept(visitorA);
+		}
+		
+		/* esto se hace recién en el segundo método!!!
+		aduana = aduana
+				+ "    		</ul>"
+				+ "    </body>"	
+				+ "</html>";	
+		*/
+		Reporte r2 = reportes.get("muelle"); //el reporte muelle con únicamente la información de importaciones
+		r1.setTexto(r1.getTexto() + muelle);
+		
+		///////////////////////Reporte aduana/////////////////////////
+			
+		///////////////////////Reporte buque/////////////////////////
+		
+		String repBuque = "<report>\n"
+					+	  "		<import>\n";
+		
+		for(Orden orden : ordenes) {
+			repBuque = repBuque + orden.accept(visitorB);
+		}
+		
+		repBuque = repBuque +	"		</import>\n";
+		
+		Reporte r3 = reportes.get("muelle"); //el reporte muelle con únicamente la información de importaciones
+		r1.setTexto(r1.getTexto() + muelle);
+		
+		///////////////////////Reporte buque/////////////////////////
 		
 	}
 
--- Terminal_Portuaria/src/ar/edu/unq/po2/generadorDeReportes/VisitorReporteBuque.java
+++ Terminal_Portuaria/src/ar/edu/unq/po2/generadorDeReportes/VisitorReporteBuque.java
@@ -1,13 +1,22 @@
 package ar.edu.unq.po2.generadorDeReportes;
 
+import ar.edu.unq.po2.container.Container;
 import ar.edu.unq.po2.orden.OrdenDeExportacion;
 import ar.edu.unq.po2.orden.OrdenDeImportacion;
 
 public class VisitorReporteBuque implements VisitorReporte {
 	
 	public String visitOrdenDeImportacion(OrdenDeImportacion orden) {
-		
+		Container c = orden.getCarga();
+		return "			<item>" + c.getIdConnteiner() + "</item>";
 	}
+	
+	/*
+	 * 	public String visitOrdenDeImportacion(OrdenDeImportacion orden) {
+		Container c = orden.getCarga();
+		return "    			<li><p> Tipo: " + c.tipo() + ", ID: " + c.getIdConnteiner() + "</p></li>\n";
+	}
+	 */
 	
 	public String visitOrdenDeExportacion(OrdenDeExportacion orden) {
 		
