548dd30fac96329446c5b06f66512ae9e6c1acd0
Fran <155672163+franngo@users.noreply.github.com>
2025-11-10 09:08:36 -0300

    Avance en la implementación de CircuitoMaritimo.

--- Terminal_Portuaria/src/ar/edu/unq/po2/circuito_maritimo/CircuitoMaritimo.java
+++ Terminal_Portuaria/src/ar/edu/unq/po2/circuito_maritimo/CircuitoMaritimo.java
@@ -1,10 +1,8 @@
 package ar.edu.unq.po2.circuito_maritimo;
 
 import java.time.Duration;
-import java.time.LocalDateTime;
 import java.util.List;
 
-import ar.edu.unq.po2.coordenada.Coordenada;
 import ar.edu.unq.po2.terminal_portuaria.TerminalPortuaria;
 import ar.edu.unq.po2.tramo.Tramo;
 
@@ -12,6 +10,8 @@ public class CircuitoMaritimo {
 	
 	private List<Tramo> tramos;
 	
+	//recordar realizar chequeo sobre la lista de Tramos recibida (deben tener sentido)
+	//puede ser con método privado que se llama al principio del constructor en el que se analiza la lista de Tramos.
 	public CircuitoMaritimo(List<Tramo> tramos) {
 		this.tramos = tramos;
 	}
@@ -32,18 +32,18 @@ public class CircuitoMaritimo {
 		return 0.0;
 	}
 	
+	/*
+	 * Observación: La razón de que se devuelva la cantidad de tramos sumada en uno es que, por ejemplo, en un circuito con
+	 * 2 tramos hay 3 terminales involucradas: la origen y la destino del primer tramo, y la origen y la destino del
+	 * segundo tramo, pero la origen del segundo tramo es la misma que la destino del primero, así que el total de terminales
+	 * diferentes del circuito es de 3.
+	 */
 	public int cantidadDeTerminales() {
-		//TODO
-		//devuelvo cualquier cosa porque necesito la interfaz definida para poder mockear la clase en los tests
-		return 0;
+		return this.tramos.size()+1;
 	}
 	
 	public TerminalPortuaria puertoDestino() {
-		//TODO
-		//devuelvo cualquier cosa porque necesito la interfaz definida para poder mockear la clase en los tests
-		/*Sería agarrar el último puerto del circuito marítimo asociado a este viaje.
-		 */
-		return new TerminalPortuaria(new Coordenada(40, 40)); // Tiene un valor aleatorio para que no tire error, es necesario que una subtarea defina el destino.
+		return this.tramos.getLast().getTerminalDestino();
 	}
 	
 }	
