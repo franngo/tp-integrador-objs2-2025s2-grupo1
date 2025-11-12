e448a267541cd6e8ef5a8faca87a185e17fc7b61
sanchezmatias911 <sanchezmatias911@yahoo.com>
2025-11-12 17:42:45 -0300

    inicio de completacion test buque

---
+++ Terminal_Portuaria/test/ar/edu/unq/po2/buque/BuqueTestCase.java
@@ -0,0 +1,25 @@
+package ar.edu.unq.po2.buque;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import ar.edu.unq.po2.terminal_portuaria.TerminalPortuaria;
+
+class BuqueTestCase {
+
+	
+	Buque miBuque;
+	TerminalPortuaria terminalDumb;
+	EstadoBuque estadoBuque;
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
--- Terminal_Portuaria/test/ar/edu/unq/po2/container/DryTestCase.java
+++ Terminal_Portuaria/test/ar/edu/unq/po2/container/DryTestCase.java
@@ -2,7 +2,11 @@ package ar.edu.unq.po2.container;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -16,10 +20,12 @@ class DryTestCase {
 	Dry containerDry;
 	CargaBL cargaBL;
 	
-	
+	ConcreteVisitorContainer visitanteContainer;
 	
 	@BeforeEach
 	void setUp()  {
+		visitanteContainer = mock(ConcreteVisitorContainer.class);
+		
 		cargaBL= mock(CargaBLCompuesto.class);
 		when(cargaBL.tipoCargaBL()).thenReturn("Carga compuesta por 10 Bl's");
 		when(cargaBL.dueños()).thenReturn("Matias,Benja,Franco,Alonso");
@@ -36,9 +42,23 @@ class DryTestCase {
 	void testDueniosCarga() {
 		assertEquals("Matias,Benja,Franco,Alonso",containerDry.getDueños());
 	}
-	
+	@Test
 	void testPesoContainerDry() {
 		assertEquals(2000d,containerDry.getPeso());
+		
 	}
+	@Test
+	void containerAceptaVisitante() {
+		when(visitanteContainer.serviciosDry(containerDry)).thenReturn(List.of());
+		containerDry.acceptVisitor(visitanteContainer);
+		verify(visitanteContainer,times(1)).serviciosDry(containerDry);
+		
+		
+	}
+	@Test
+	void laCargaEsCoherente() {
+		assertEquals(cargaBL,containerDry.carga());
+	}
+	
 
 }
